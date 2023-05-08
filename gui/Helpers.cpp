af9f9494e710e0b02afa58a281ea0f23b2aebaf5
Brage <brage@bel.desi>
2023-05-08 11:16:50 +0200

    First working commit

--- gui/Helpers.cpp
+++ gui/Helpers.cpp
@@ -96,4 +96,158 @@ namespace gui
         }        
         return res;
     }
+
+    bool LoadSettings(Settings &settings, std::string filename)
+    {            
+        //std::cout << "Filename: " << filename << std::endl;
+        std::ifstream file(filename);    
+        std::map<std::string, std::string> settingsMap;    
+
+        if(file) //config file exists
+        {
+            file.seekg(0,std::ios::end);
+            std::streampos length = file.tellg();
+            file.seekg(0,std::ios::beg);
+
+            // Read in to buffer
+            std::vector<char>       buffer(length);
+            file.read(&buffer[0],length);
+            file.close();
+
+            // Parse buffer to stream
+            std::stringstream       localStream;
+            localStream.rdbuf()->pubsetbuf(&buffer[0],length);
+
+            std::string line;
+            while( std::getline(localStream, line) )
+            {
+                std::istringstream is_line(line);
+                std::string key;
+                if( std::getline(is_line, key, '=') ) //gets the part before "="
+                {
+                    std::string value;
+                    if( std::getline(is_line, value) ) //gets the part after "="
+                    {
+                        //std::cout << "key " << key << ", value " << value << std::endl;
+                        settingsMap.insert({key,value});
+                    }                        
+                    //store_line(key, value);
+                }
+            }            
+        } else
+        {
+            std::cout << "Could not read config file" << std::endl;
+            //Create config file
+            std::vector<std::string> lines;
+            CreateConfig(lines, settingsMap);
+            
+            //Write all lines to file
+            std::ofstream file(filename, std::ios::out | std::ios::trunc);
+            for(const auto& i : lines) {
+                file << i << std::endl;
+            }
+            file.close();            
+        }
+
+        //Go through settings and add to settings struct
+        for (auto p : settingsMap)
+        {
+            //std::cout << "p1: " << p.first << ", p2: " << p.second << std::endl;
+            if(p.first.compare("path") == 0)
+            {
+                std::string localPath = "";
+                //Check if we need to parse environment variable                
+                if(p.second.find("$") != std::string::npos)
+                {
+                    // We assume $ is first character
+                    p.second.erase(0,1);                    
+                    char * val = std::getenv(p.second.c_str());
+                    if (val == NULL)
+                        localPath = "";
+                    else
+                        localPath = std::string(val);
+                }
+                else
+                    localPath = p.second;
+                //std::cout << "Path: " << localPath << std::endl;
+                settings.defaultPath = localPath;
+            } else if (p.first.compare("updatePath") == 0)
+            {
+                if(p.second.compare("True") == 0)
+                    settings.updatePath = true;
+                else
+                    settings.updatePath = false;
+            }
+        }
+        
+        return true;
+
+    }
+
+    bool SetSettingsPath(std::string path, std::string filename)
+    {
+        //Remove filename from path
+        std::size_t lastBackSlash = path.find_last_of("/\\");
+        std::string localPath =  path.substr(0,lastBackSlash);
+        
+        //Open file and find path line
+        std::ifstream file(filename);    
+        if(file) //config file exists
+        {
+            file.seekg(0,std::ios::end);
+            std::streampos length = file.tellg();
+            file.seekg(0,std::ios::beg);
+
+            // Read in to buffer
+            std::vector<char>       buffer(length);
+            file.read(&buffer[0],length);
+            file.close();
+
+            // Parse buffer to stream
+            std::stringstream       localStream;
+            localStream.rdbuf()->pubsetbuf(&buffer[0],length);
+
+            std::string line;
+            std::vector<std::string> lines;
+            while( std::getline(localStream, line) )
+            {
+                std::istringstream is_line(line);
+                std::string key;
+                if( std::getline(is_line, key, '=') ) //gets the part before "="
+                {
+                    if(key.compare("path") == 0)
+                    {
+                        line = "path="+localPath;
+                    }                                            
+                }
+                lines.push_back(line);
+            }
+            
+            //Write all lines to file
+            std::ofstream file(filename, std::ios::out | std::ios::trunc);
+            for(const auto& i : lines) {
+                file << i << std::endl;
+            }
+            file.close();
+        }
+        else
+        {
+            std::cout << "Could not update config file" << std::endl;
+        }
+        return true;
+    }
+    
+    bool CreateConfig(std::vector<std::string> &lines, std::map<std::string, std::string> &settingsMap)
+    {
+        lines.clear();
+        lines.push_back("[Directory settings]");
+        lines.push_back("path=$HOME");
+        lines.push_back("updatePath=False");
+        //Insert into map
+        settingsMap.clear();
+        settingsMap.insert({"path","$HOME"});
+        settingsMap.insert({"updatePath","false"});
+
+        return true;
+    }
 } // namespace gui
